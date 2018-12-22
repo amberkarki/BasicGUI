34c9dafe71a2bf35fad12c96ddef5b6efe6888be
Amber <amberkarki@gmail.com>
2018-12-22 13:55:41 +0200

    First commit

---
+++ src/BasicGUIconcept.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane; // importing   a class from javax.swing 
+
+public class BasicGUIconcept {
+
+	public static void main(String[] args) {
+		
+		
+		
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "This is my First GUI Test in Java");
+		// JOptionPane // is one of the predefined  classes in java system.
+		// showMessageDialog method  will be invoked with two argument 
+		//null and message null first argument is a java keyword   better concept will be in  the object and class 
+
+	}
+
+}
